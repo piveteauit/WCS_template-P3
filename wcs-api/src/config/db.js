e7424d8dfeb2536a93b96ceaeaa686cab0556c8f
romain.piveteau <r.piveteau@outlook.com>
2023-05-30 16:00:47 +0200

    dotenv added

--- wcs-api/src/config/db.js
+++ wcs-api/src/config/db.js
@@ -1,12 +1,10 @@
+require('dotenv').config();
+
 const mysql = require('mysql2/promise');
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = {
-    DB_HOST: "localhost",
-    DB_USER: "express",
-    DB_PASSWORD: "express",
-    DB_NAME: "express_quests"
-};
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
+// console.log(DB_HOST, DB_USER)
 const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
