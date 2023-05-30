2bdd0fa77774fe3bfecede701be83b16a834b71f
romain.piveteau <r.piveteau@outlook.com>
2023-05-30 16:43:01 +0200

    Dynamique model

--- wcs-api/src/controllers/BaseController.js
+++ wcs-api/src/controllers/BaseController.js
@@ -2,10 +2,21 @@ class BaseController {
 
     req;
     res;
+    model;
 
     constructor(req, res) {
         this.req = req;
         this.res = res;
+    }
+
+    getAll() {
+        this.model.getAll()
+            .then(([results]) => this.sendJson(results))
+    }
+
+    getById() {
+        this.model.getById(this.req.params.id)
+            .then(([results]) => this.sendJson(results))
     }
 
     sendJson(data) {
