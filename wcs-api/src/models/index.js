a3683cb3965328874c674673f91c764e60edb69a
Gabriel Pilaud <gabriel.pilaud@hotmail.fr>
2023-07-05 10:36:10 +0200

    index-api-ok

--- wcs-api/src/models/index.js
+++ wcs-api/src/models/index.js
@@ -1,9 +1,20 @@
 const TastesModel = require("./TastesModel")
-const UserModel = require("./UserModel")
-const YearsModel = require("./yearsModel")
+const UserModel = require("./UserModel");
+const YearsModel = require("./yearsModel");
+const BasesModel = require("./BaseModel");
+const CepagesModel = require("./CepagesModel");
+const ColorsModel = require("./ColorsModel");
+const RolesModel = require("./RolesModel");
+const TerroirsModel = require("./TerroirsModel");
+
 
 module.exports = {
     UserModel,
     TastesModel,
     YearsModel,
+    BasesModel,
+    CepagesModel,
+    ColorsModel,
+    RolesModel,
+    TerroirsModel
 }
