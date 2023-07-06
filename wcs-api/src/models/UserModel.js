66f943cb86eb494275f503a6ba2c378e12c5158d
Coopykat <kathycamazon@gmail.com>
2023-07-06 14:07:35 +0200

    route create delete and update user

--- wcs-api/src/models/UserModel.js
+++ wcs-api/src/models/UserModel.js
@@ -7,8 +7,15 @@ class UserModel extends BaseModel {
     }
 
     insert(userData) {
-        console.log(userData)
         return this.db.query('INSERT INTO users (role_id, date, name, age, mail, password)  VALUES (?, ?, ?, ?, ?, ?)', [2, userData.date, userData.name, userData.age, userData.mail, userData.password]);
+    }
+
+    deleteUser(id) {
+        return this.db.query('DELETE FROM users WHERE id = ?', [id])
+    }
+
+    updateUser(userData) {
+        return this.db.query('UPDATE users SET role_id = ?, date = ?, name = ?, age = ?, mail = ?, password = ? WHERE id = ?', [2, userData.date, userData.name, userData.age, userData.mail, userData.password, userData.id]);
     }
 }
 
