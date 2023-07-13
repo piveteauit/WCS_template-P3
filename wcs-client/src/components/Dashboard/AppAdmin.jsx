67d017f3b4b65049ad9c704b299eeb0fb8028aaa
Gabriel Pilaud <gabriel.pilaud@hotmail.fr>
2023-07-13 16:25:39 +0200

    dashboard

---
+++ wcs-client/src/components/Dashboard/AppAdmin.jsx
@@ -0,0 +1,12 @@
+import '../../styles/Sidebar.css';
+import Sidebar from "../Dashboard/Sidebar";
+
+function AppAdmin() {
+    return (
+        <div className="AppAdmin">
+            <Sidebar/>
+        </div>
+    );
+}
+
+export default AppAdmin; 
