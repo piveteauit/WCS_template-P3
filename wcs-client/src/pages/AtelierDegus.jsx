c560a3012deeed7635e9b47ebc36c8f8dfe45355
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-07-17 17:17:50 +0200

    css details

--- wcs-client/src/pages/AtelierDegus.jsx
+++ wcs-client/src/pages/AtelierDegus.jsx
@@ -17,9 +17,21 @@ const AtelierDegus = () => {
                 <h1>Atelier Dégustation</h1> 
             </div>
 
-            <p>Atelier dégustation de 4 vins a l'aveugle suivi d'accompagnement de Mets .
-                Nos ateliers <span className='inovin'>INOVIN</span> sont conçus pour vous offrir une expérience inoubliable, alliant découverte, apprentissage et convivialité.
-                Vous serez guidé par des experts passionnés et compétents, qui vous feront découvrir les secrets du vin. </p>
+            <p>Plongez dans l'univers fascinant de la dégustation de vin lors de notre atelier dégustation. </p>
+
+            <p>Nos ateliers <span className='inovin'>INOVIN</span> ont été soigneusement conçus pour vous offrir une expérience inoubliable, combinant découverte, apprentissage et convivialité. Sous la guidance d'experts passionnés et compétents, vous plongerez dans les secrets de l'art de la dégustation. </p>
+
+<p>Lors de cet atelier, vous aurez l'occasion de déguster quatre vins soigneusement sélectionnés, et ce, à l'aveugle. Cette expérience unique mettra à l'épreuve vos sens et vous permettra de développer votre palais en reconnaissant les arômes et les saveurs.
+
+Pour compléter cette expérience sensorielle, chaque vin sera accompagné de mets savoureux, spécialement choisis pour mettre en valeur les caractéristiques de chaque vin. Cette harmonie entre le vin et les mets ajoutera une dimension gustative exceptionnelle à votre dégustation. </p>
+
+<p>Que vous soyez une entreprise à la recherche d'un événement mémorable, un groupe de collègues désireux de renforcer leurs liens ou un amateur passionné en quête de connaissances, notre atelier <span className='inovin'>INOVIN</span> est adapté à tous.</p>
+
+<p>Êtes-vous prêts à embarquer pour cette aventure gustative ? N'hésitez pas à nous contacter pour en savoir davantage sur notre offre <span className='inovin'>INOVIN</span> !
+
+Nous avons hâte de partager avec vous notre passion pour la dégustation de vin.
+
+À très bientôt !</p>
                 
             <Link to="/inscription" className='inscription-btn'>S'inscrire</Link>
             
